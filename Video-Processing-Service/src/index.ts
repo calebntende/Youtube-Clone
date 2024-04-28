d722cfb09c3246214e0987af3e070820f7c61159
calebntende <CalebNtende@my.unt.edu>
2024-04-27 23:09:41 -0500

    adding video processing service

--- Video-Processing-Service/src/index.ts
+++ Video-Processing-Service/src/index.ts
@@ -1,10 +1,17 @@
-import express from 'express';
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post("/process-video", (req, res) => {
+  //get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if(!inputFilePath || !outputFilePath) {
+    res.status(400).send("Please provide input and output file paths");
+  }
 });
 
 app.listen(port, () => {
